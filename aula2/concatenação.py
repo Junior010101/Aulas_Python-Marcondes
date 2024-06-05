484a9ab11966cfe6684b2de613eb9eb7c29162f3
Junior010101 <marcondesjunior2209@gmail.com>
2024-06-05 14:35:11 -0300

    commit

---
+++ aula2/concatenação.py
@@ -0,0 +1,5 @@
+s1 = "texto" #delimitada por aspas sinples ou duplas
+s3 = '0.5' #a3 é do tipo float
+a3 = 0.5
+n1 = s1 + s3
+print(n1)
---
+++ aula2/texte.py
@@ -0,0 +1,6 @@
+nome = "Marcondes"
+idade = 15
+altura = 1.76
+maior_de_idade = idade>= 18
+print("Nome:", nome, "Idade:", idade, "Altura:", altura)
+print("É maior de Idade?", maior_de_idade)
