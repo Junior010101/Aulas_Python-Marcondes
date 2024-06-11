8e327f5fdb9a92d3f3ce3665a6273972e68dba9f
Junior010101 <marcondesjunior2209@gmail.com>
2024-06-10 21:52:45 -0300

    Att

--- Aulas/aula2/concatenação.py
+++ Aulas/aula2/concatenação.py
@@ -1,5 +1,7 @@
-s1 = "texto" #delimitada por aspas sinples ou duplas
-s3 = '0.5' #a3 é do tipo float
-a3 = 0.5
-n1 = s1 + s3
+s1 = "texto" #delimitada por aspas sinples ou duplas == String.
+s3 = 5 #s3 == Number/inteiro.
+a3 = 0.5 #a3 == float/decimal.
+a4 = 'texte'
+
+n1 = a4 + " de " + s1
 print(n1)
