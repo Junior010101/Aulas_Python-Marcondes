aa8d49d337cfab89aec16eccd65b8800f3377d3b
Junior010101 <marcondesjunior2209@gmail.com>
2024-06-12 14:46:17 -0300

    Adição da atividade 2

---
+++ Atividades/atv-2.py
@@ -0,0 +1,5 @@
+#Objetivo da Atividade era caucular um aumento de 5% em um salario de R$1.765,43
+salario = 1765.43
+aumento = salario * 5 / 100 #Aumento == Salario, vezes 5, dividido por 100
+salario_final = salario + aumento #Salario Final == salario + aumento
+print(round(salario_final)) #Função round, abrevia o valor final
